6ea776a23e4dc26c9b04a12b58517ce1bd00a185
anish-singh-rawat <anishsinghrawat1>
2023-11-02 19:12:14 +0530

    first commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
 
-export default function Todo() {
+import React, { useEffect, useState } from 'react'
+import Form from './Form'
+import TodosList from './TodosList';
+
+const getLocalItems =()=>{
+  let list = localStorage.getItem('lists')
+  if(list){
+    return JSON.parse(localStorage.getItem('lists'))
+  }
+  else{
+    return []
+  }
+  
+}
+
+const Todo = () => {
+    const [input,setInput] = useState('');              
+  const [todos,setTodos] = useState(getLocalItems());   
+  const [editTodo,setEditTodo] =useState(null);         
+  const [isEdit,setIsEdit] = useState(false);        
+  const [errorMessage, setErrorMessage] = useState('');
+  const [completed,setCompleted] =useState(false);
+
+  useEffect(()=>{
+    localStorage.setItem('lists',JSON.stringify(todos))
+    
+},[todos])
+
   return (
-    <div>Todo</div>
+    <div className='parent'>
+      <div className='child'>
+        <div className='container'>
+        <Form 
+          input = {input} 
+          setInput = {setInput} 
+          todos = {todos} 
+          setTodos = {setTodos} 
+          editTodo={editTodo} 
+          setEditTodo={setEditTodo}
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+        />
+
+        <TodosList 
+          todos={todos} 
+          setTodos={setTodos} 
+          editTodo={editTodo}
+          setEditTodo={setEditTodo} 
+          setInput={setInput}
+          isEdit={isEdit}
+          setIsEdit={setIsEdit}
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+          completed={completed}
+          setCompleted={setCompleted}
+        />
+          </div>
+      </div>
+    </div>
   )
 }
+
+export default Todo
+
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,7 +1,130 @@
-import React from 'react'
 
-export default function TodosList() {
+
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare, faTrash, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { data } from '../staticData/buttonData';
+
+const TodosList = ({ todos, setTodos, editTodo, setEditTodo, isEdit, setIsEdit, errorMessage, setErrorMessage }) => {
+  const [editedText, setEditedText] = useState('');
+  const [filter, setFilter] = useState('All'); 
+  const [active, setActive] = useState(null)  
+
+  const handleFilter = (id,name) => {
+    setFilter(name)
+    setActive(id)
+  };
+
+
+  const filterTodos = () => {
+    if (filter === 'All') {
+      return todos;
+    } else if (filter === 'Completed') {
+      return todos.filter((todo) => todo.completed);
+    } else if (filter === 'Incomplete') {
+      return todos.filter((todo) => !todo.completed);
+    }
+  };
+
+  const filteredTodos = filterTodos();
+
+
+  const handleComplete = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const onChangeInput = (e) => {
+    setEditedText(e.target.value);
+    setErrorMessage('');
+  };
+
+  const handleEdit = (id) => {
+    setIsEdit(true);
+    setEditTodo(id);
+    const editedTodo = todos.find((todo) => todo.id === id);
+    setEditedText(editedTodo.title);
+  };
+
+  const handleSave = () => {
+    const trimmedText = editedText.trim();
+    if (trimmedText !== '') {
+      const updatedTodos = todos.map((todo) =>
+        todo.id === editTodo ? { ...todo, title: trimmedText } : todo
+      );
+      setTodos(updatedTodos);
+    } else {
+      setErrorMessage('Todo cannot be empty');
+    }
+    setEditTodo(null);
+    setIsEdit(false);
+  };
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div>TodosList</div>
-  )
-}
+    <div>
+      <div className='status'>
+      {
+        data.map((ele)=>{
+          return(
+            <button className={ele.id===active?"active":"status-btn"} onClick={()=>handleFilter(ele.id,ele.name)} key={ele.id}>{ele.name}</button>
+          )
+        })
+      }
+      </div>
+      {filteredTodos.map((todo) => (
+        <div className='display-todos' key={todo.id}>
+          <li className='list'>
+            <div className='single-todo'>
+            <div className='checkbox-text'>
+            <input
+                className='check-box'
+                type='checkbox'
+                checked={todo.completed}
+                onChange={() => handleComplete(todo.id)}
+              />
+              {isEdit && editTodo === todo.id ? (
+                <input
+                  type='text'
+                  className='todos-input-field'
+                  value={editedText}
+                  onChange={onChangeInput}
+                />
+              ) : (
+                todo.title
+              )}</div>
+              {todo.completed == true ? <div className='done-msg'>Completed</div> :""}
+              
+            </div>
+            {isEdit && editTodo === todo.id ? (
+              <button className='btn' onClick={handleSave}>
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+            ) : (
+              <>
+                <button className='btn' onClick={() => handleEdit(todo.id)}>
+                  <FontAwesomeIcon icon={faPenToSquare} />
+                </button>
+                <button className='btn' onClick={() => handleDelete(todo)}>
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </>
+            )}
+          </li>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodosList;
+
+
+
+
+
