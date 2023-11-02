12901e5236faaf33309ec217199fc1e7fadcf221
anish-singh-rawat <anishsinghrawat1>
2023-11-02 18:42:22 +0530

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo,errorMessage,setErrorMessage }) => {
+    
+
+    const onInputChange = (e) => {
+        setInput(e.target.value);
+        setErrorMessage('');
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        const trimmedInput = input.trim();
+        if (!editTodo && trimmedInput !== '') {
+            setTodos([...todos, { id: uuidv4(), title: trimmedInput, completed:false }]);
+            setInput('');
+        } else {
+            setErrorMessage('please enter data');
+        }
+    };
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <div className="todo-heading">Todo APP </div>
+            <br />
+            <input type="text" className='form-input-field' placeholder='Enter todo here...' required value={input} onChange={onInputChange} />
+            <button className='btn' type='submit'><FontAwesomeIcon icon={faPlus} /></button>
+            {errorMessage && <p className='error-message'>{errorMessage}</p>}
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/staticData/buttonData/index.jsx
@@ -0,0 +1,5 @@
+export const data = [
+  { id: "1", name: "All" },
+  { id: "2", name: "Completed" },
+  { id: "3", name: "Incomplete" },
+];
