e09e60630303f3e897fed08575dfc07175126e59
anish-singh-rawat <anishsinghrawat1>
2023-11-03 11:05:57 +0530

    Updated TODO APP

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
-export default function Form() {
-  return (
-    <div>Form</div>
-  )
-}
+const Form = ({ inputData, setInputData, todos, setTodos, editTodo, setEditTodo,errorMessage,setErrorMessage }) => {
+    
+
+    const onInputChange = (e) => {
+        setInputData(e.target.value);
+        setErrorMessage('');
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        const trimmedInput = inputData.trim();
+        if (!editTodo && trimmedInput !== '') {
+            setTodos([...todos, { id: uuidv4(), title: trimmedInput, completed:false }]);
+            setInputData('');
+        } else {
+            setErrorMessage('please enter data');
+        }
+    };
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <div className="todo-heading">Todo APP </div>
+            <br />
+            <input type="text" className='form-input-field' placeholder='Enter todo here...' required value={inputData} onChange={onInputChange} />
+            <button className='btn' type='submit'><FontAwesomeIcon icon={faPlus} /></button>
+            {errorMessage && <p className='error-message'>{errorMessage}</p>}
+        </form>
+    );
+};
+
+export default Form;
